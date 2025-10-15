b81d83a9925fa5aa5d74abc612e9332c2490604c
Ha-minsang <gkalstkd@gmail.com>
2025-10-15 14:00:32 +0900

    exit 기능 추가

--- src/myCalculator/Main.java
+++ src/myCalculator/Main.java
@@ -1,4 +1,17 @@
 package myCalculator;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            String input = sc.nextLine();
+            if(input.equals("exit")){
+                System.out.println("계산기를 종료합니다.");
+                break;
+            }
+
+    }
+    }
 }
