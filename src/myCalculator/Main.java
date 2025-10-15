b26f7f843dd84f5e4c60f0d37de2267991822944
Ha-minsang <gkalstkd@gmail.com>
2025-10-15 16:30:22 +0900

    1단계 계산기 기능 추가 - 양의 정수끼리의 사칙 연산 - 0으로 나누기 시도시 메세지 출력 - 계산이 끝날때 종료할 것인지 질문, exit를 입력하지 않으면 계산기 반복 실행

--- src/myCalculator/Main.java
+++ src/myCalculator/Main.java
@@ -5,13 +5,52 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while(true){
+        while (true) {
+
+            System.out.print("첫번째 숫자를 입력하세요: ");
+            int number1 = sc.nextInt();
+            if (number1 < 0) {
+                System.out.println("양의 정수를 입력하세요.");
+            }
+
+            System.out.print("두번째 숫자를 입력하세요: ");
+            int number2 = sc.nextInt();
+            if (number2 < 0) {
+                System.out.println("양의 정수를 입력하세요.");
+            }
+
+            System.out.print("연산 기호를 입력하세요: ");
+            char operation = sc.next().charAt(0);
+            switch (operation) {
+                case '+':
+                    System.out.println(number1 + number2);
+                    break;
+                case '-':
+                    System.out.println(number1 - number2);
+                    break;
+                case '*':
+                    System.out.println(number1 * number2);
+                    break;
+                case '/':
+                    if (number2 == 0) {
+                        System.out.println("0으로는 나눌수 없습니다.");
+                    } else {
+                        System.out.println(number1 / number2);
+                    }
+                    break;
+                default:
+                    System.out.println("잘못된 연산 기호입니다.");
+            }
+            sc.nextLine();
+            System.out.print("계산기를 종료 하시겠습니까?(exit를 입력하면 종료됩니다)");
+
             String input = sc.nextLine();
-            if(input.equals("exit")){
+            if (input.equals("exit")) {
                 System.out.println("계산기를 종료합니다.");
                 break;
             }
 
-    }
+
+        }
     }
 }
