01c69ccc5f43a21f71da0507f85990b48ce8b8e2
Ha-minsang <gkalstkd@gmail.com>
2025-10-16 10:15:24 +0900

    게터, 세터 메서드 추가 ArrayList의 final 속성 제거

--- src/myCalculator/Calculator.java
+++ src/myCalculator/Calculator.java
@@ -3,11 +3,13 @@ package myCalculator;
 import java.util.ArrayList;
 
 public class Calculator {
-    private static final ArrayList<Integer> answers = new ArrayList<>();
+    private static ArrayList<Integer> answers = new ArrayList<>();
 
     // 덧셈 기능
     static void add(int a, int b) {
         int result = a + b;
+        answers.add(result);
+
         System.out.println("계산 결과는 " + result + "입니다.");
     }
 
@@ -15,6 +17,7 @@ public class Calculator {
     static void sub(int a, int b) {
         int result = a - b;
         answers.add(result);
+
         System.out.println("계산 결과는 " + result + "입니다.");
     }
 
@@ -22,6 +25,7 @@ public class Calculator {
     static void mul(int a, int b) {
         int result = a * b;
         answers.add(result);
+
         System.out.println("계산 결과는 " + result + "입니다.");
     }
 
@@ -29,11 +33,16 @@ public class Calculator {
     static void div(int a, int b) {
         int result = a / b;
         answers.add(result);
+
         System.out.println("계산 결과는 " + result + "입니다.");
     }
 
     public static ArrayList<Integer> getAnswers() {
         return answers;
+    }
+
+    public static void setAnswers(ArrayList<Integer> answers) {
+        Calculator.answers = answers;
     }
 
 }
