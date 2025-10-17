858dba265642aea1b09ff3c0b9de80650d8f5bd1
Ha-minsang <gkalstkd@gmail.com>
2025-10-17 17:00:52 +0900

    사칙 연산 메서드들 Enum으로 이동으로 인한 삭제 / addAnswer 메서드 추가

--- src/myCalculator/Calculator.java
+++ src/myCalculator/Calculator.java
@@ -5,38 +5,6 @@ import java.util.ArrayList;
 public class Calculator {
    private ArrayList<Integer> answers = new ArrayList<>();
 
-    // 덧셈 기능
-    void add(int a, int b) {
-        int result = a + b;
-        answers.add(result);
-
-        System.out.println("계산 결과는 " + result + "입니다.");
-    }
-
-    // 뺄셈 기능
-    void sub(int a, int b) {
-        int result = a - b;
-        answers.add(result);
-
-        System.out.println("계산 결과는 " + result + "입니다.");
-    }
-
-    // 곱셈 기능
-    void mul(int a, int b) {
-        int result = a * b;
-        answers.add(result);
-
-        System.out.println("계산 결과는 " + result + "입니다.");
-    }
-
-    // 나눗셈 기능
-    void div(int a, int b) {
-        int result = a / b;
-        answers.add(result);
-
-        System.out.println("계산 결과는 " + result + "입니다.");
-    }
-
     // 게터 기능
     public ArrayList<Integer> getAnswers(){
         return answers;
@@ -45,6 +13,10 @@ public class Calculator {
     // 세터 기능
     public void setAnswers(ArrayList<Integer> answers) {
         this.answers = answers;
+    }
+
+    public void addAnswer(int result) {
+        answers.add(result);
     }
 
     // 삭제 기능
