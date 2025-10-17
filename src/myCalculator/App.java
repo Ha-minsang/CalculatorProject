12083d9f0e70f0b76dddc491a283b5e14861025a
Ha-minsang <gkalstkd@gmail.com>
2025-10-17 09:29:38 +0900

    1. answers의 마지막 값을 받아오도록 게터 사용(값이 없을때 발생되는 에러 캐치로 잡아서 다음 코드 진행되게함)
    2. number1,number2를 입력값으로 받는게 아닌 다른 값으로도 사용할수 있도록 반복문 밖에 int로 작성

--- src/myCalculator/App.java
+++ src/myCalculator/App.java
@@ -8,57 +8,67 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Calculator cal = new Calculator();
+        int number1 = 0;
+        int number2 = 0;
 
         while (true) {
-            System.out.print("첫번째 숫자를 입력하세요: ");
             try {
-                int number1 = sc.nextInt();
-                if (number1 < 0) {
-                    System.out.println("양의 정수를 입력하세요.");
-                }
-
-                System.out.print("두번째 숫자를 입력하세요: ");
-                int number2 = sc.nextInt();
-                if (number2 < 0) {
-                    System.out.println("양의 정수를 입력하세요.");
-                }
-
-                System.out.print("연산 기호를 입력하세요: ");
-                sc.nextLine();
-                String operation = sc.nextLine();
-                switch (operation) {
-                    case "+":
-                        cal.add(number1, number2);
-                        break;
-                    case "-":
-                        cal.sub(number1, number2);
-                        break;
-                    case "*":
-                        cal.mul(number1, number2);
-                        break;
-                    case "/":
-                        if (number2 == 0) {
-                            System.out.println("0으로는 나눌수 없습니다.");
-                        } else {
-                            cal.div(number1, number2);
-                        }
-                        break;
-                    default:
-                        System.out.println("잘못된 연산 기호입니다.");
-                }
-                System.out.print("더 계산하시겠습니까?(exit를 입력하면 종료됩니다)");
-                String input = sc.nextLine();
-
-                if (input.equals("exit")) {
-                    System.out.println("계산기를 종료합니다.");
-                    break;
-                }
+                int calNumber = cal.getAnswers().get(cal.getAnswers().size()-1);
             }
-            catch (InputMismatchException e) {
-                sc.nextLine();
-                System.out.println("정상적인 입력이 아닙니다.");
-            }
+            catch (IndexOutOfBoundsException e) {} // 에러가 발생해도 다음 코드 실행
 
+                System.out.print("첫번째 숫자를 입력하세요: ");
+                try {
+                    number1 = sc.nextInt();
+                    if (number1 < 0) {
+                        System.out.println("양의 정수를 입력하세요.");
+                        break;
+                    }
+
+                    System.out.print("두번째 숫자를 입력하세요: ");
+                    number2 = sc.nextInt();
+                    if (number2 < 0) {
+                        System.out.println("양의 정수를 입력하세요.");
+                        break;
+                    }
+
+                    System.out.print("연산 기호를 입력하세요: ");
+                    sc.nextLine();
+                    String operation = sc.nextLine();
+                    switch (operation) {
+                        case "+":
+                            cal.add(number1, number2);
+                            break;
+                        case "-":
+                            cal.sub(number1, number2);
+                            break;
+                        case "*":
+                            cal.mul(number1, number2);
+                            break;
+                        case "/":
+                            if (number2 == 0) {
+                                System.out.println("0으로는 나눌수 없습니다.");
+                            } else {
+                                cal.div(number1, number2);
+                            }
+                            break;
+                        default:
+                            System.out.println("잘못된 연산 기호입니다.");
+                    }
+                    System.out.print("더 계산하시겠습니까?(exit를 입력하면 종료됩니다)");
+                    String input = sc.nextLine();
+
+                    if (input.equals("exit")) {
+                        System.out.println("계산기를 종료합니다.");
+                        break;
+                    }
+                }
+                catch (InputMismatchException e) {
+                    sc.nextLine();
+                    System.out.println("정상적인 입력이 아닙니다.");
+                }
+
+            }
         }
     }
-}
+
