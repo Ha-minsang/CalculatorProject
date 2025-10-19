b1cf50158185b871c9bb19dec4e1a8480909b784
Ha-minsang <gkalstkd@gmail.com>
2025-10-19 16:48:19 +0900

    정수일 경우 소수점을 지워주고 소수일 경우 소수점 아래 3자리까지만 표시해주는 클래스 생성

---
+++ src/myCalculator/core/NumberFormat.java
@@ -0,0 +1,14 @@
+package myCalculator.core;
+
+public class NumberFormat {
+    public String changeType(double numberDouble) {
+        if (numberDouble % 1 == 0) {
+            return String.valueOf((int) numberDouble);
+        }
+        return String.valueOf(Math.ceil(numberDouble*1000) / 1000);
+    }
+}
+
+
+
+
