b1dc9d12bd30a9000bc4b4ef6d03ed3b8de8781b
Ha-minsang <gkalstkd@gmail.com>
2025-10-19 12:03:51 +0900

    사칙 연산들을 람다식으로 변환
    2개의 숫자를 받으면 람다식의 결과값을 반환받는 메서드 생성
    
    입력된 기호와 사칙연산별로 지정된 심볼이 일치할때 람다식을 불러오도록 메서드 생성

--- src/myCalculator/core/Operation.java
+++ src/myCalculator/core/Operation.java
@@ -1,38 +1,42 @@
 package myCalculator.core;
 
+import java.util.function.DoubleBinaryOperator;
+
 public enum Operation {
     // 덧셈 기능
-    ADD("+") {
-        public double apply(double a, double b) {
-            return a + b;
-        }
-    },
+    ADD("+", (a, b) -> a + b),
 
     // 뺄셈 기능
-    SUB("-") {
-        public double apply(double a, double b) {
-            return a - b;
-        }
-    },
+    SUB("-", (a, b) -> a - b),
 
     // 곱셈 기능
-    MUL("*") {
-        public double apply(double a, double b) {
-            return a * b;
-        }
-    },
+    MUL("*",  (a, b) -> a * b) ,
 
     // 나눗셈 기능
-    DIV("/") {
-        public double apply(double a, double b) {
-            return a / b;
-        }
-    };
+    DIV("/" , (a, b) -> a / b);
 
-String symbol;
-    Operation(String symbol) {
+    String symbol;
+    DoubleBinaryOperator operator;
+
+    Operation(String symbol, DoubleBinaryOperator operator) {
         this.symbol = symbol;
+        this.operator = operator;
     }
 
-public abstract double apply(double a, double b);
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public double calculate(double a, double b) {
+        return operator.applyAsDouble(a, b);
+    }
+
+    public static Operation formSymbol(String symbol) {
+        for(Operation operation : Operation.values()) {
+            if(operation.getSymbol().equals(symbol)) {
+                return operation;
+            }
+        }
+        throw new IllegalArgumentException("지원하지 않는 연산 기호입니다.");
+    }
 }
