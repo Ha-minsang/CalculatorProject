0d787e836c0733ac5fa27e81ad6e0f895bd1ab9d
Ha-minsang <gkalstkd@gmail.com>
2025-10-19 16:48:38 +0900

    정수일 경우 소수점을 지워주고 소수일 경우 소수점 아래 3자리까지만 표시되게 변경

--- src/myCalculator/App.java
+++ src/myCalculator/App.java
@@ -1,6 +1,7 @@
 package myCalculator;
 
 import myCalculator.core.InputManager;
+import myCalculator.core.NumberFormat;
 import myCalculator.core.Operation;
 import myCalculator.core.ResultManager;
 
@@ -8,6 +9,7 @@ public class App {
     public static void main(String[] args) {
         ResultManager resultManager = new ResultManager();
         InputManager inputManager = new InputManager();
+        NumberFormat numberFormat = new NumberFormat();
 
         double number1 = 0;
         double number2 = 0;
@@ -15,12 +17,11 @@ public class App {
 //        double resultNumber =result; // 마지막으로 저장된 계산 결과
         double menu = 1;
         int exit = 0;
+        String displayResult = "";
 
         System.out.println("계산기를 실행합니다.");
 
-        while (true) {
-            if(exit==1) break;
-
+        while (exit != 1) {
             switch ((int) menu) {
                 // 새로운 계산
                 case 1: {
@@ -31,43 +32,39 @@ public class App {
 
                 // 이어서 계산
                 case 2: {
-                    try {
-//                        resultNumber = (double) resultManager.getAnswers().get(resultManager.getAnswers().size() - 1);
-                        number1 = result;
-                        System.out.println("\n첫번째 숫자는 이전 계산 결과인 " + number1 + "입니다.");
-                    }
-
-                    // 이전 계산 결과 값이 없을 때 첫번째 숫자 입력
-                    catch (IndexOutOfBoundsException e) {
-                        System.out.print("이어서 할 계산 기록이 없습니다.");
-                        System.out.print("첫번째 숫자를 입력하세요: ");
-                        number1 = inputManager.inputNumber("firstNumber");
-                    }
+                    number1 = result;
+                    System.out.println("\n첫번째 숫자는 이전 계산 결과인 " + displayResult + "입니다.");
                     break;
                 }
 
                 // 계산 기록 불러오기
                 case 3: {
-                    System.out.println("이전 계산 결과를 불러옵니다.\n");
-                    for(Object answerHistory : resultManager.getAnswers() ) {
-                        System.out.println(answerHistory);
+                        System.out.println("\n이전 계산 기록를 불러옵니다.\n");
+                        for (Object answerHistory : resultManager.getAnswers()) {
+                            System.out.println(answerHistory);
+                        }
                     }
 
-                    System.out.print("1.메뉴 선택으로 돌아가기       " + "2.오래된 기록 삭제\n"
-                            + "3.검색 필터   " + "4.계산기 종료\n"
-                            + "원하시는 메뉴를 선택해 숫자를 입력해주세요: ");
-                    double select =  inputManager.inputNumber("menuNumber");
+                    System.out.print("\n1.메뉴 선택으로 돌아가기" + "\n2.오래된 기록 삭제"
+                            + "\n3.검색 필터" + "\n4.계산기 종료\n"
+                            + "\n원하시는 메뉴를 선택해 숫자를 입력해주세요: ");
+                    double select = inputManager.inputNumber("menuNumber");
 
-                    switch ((int)select) {
+                    switch ((int) select) {
                         case 1: {
-                            System.out.print("1.새로운 계산       " + "2.이어서 계산\n"
-                                    + "3.계산 기록 보기   " + "4.계산기 종료\n"
-                                    + "원하시는 메뉴를 선택해 숫자를 입력해주세요: ");
+                            System.out.print("\n1.새로운 계산" + "\n2.이어서 계산"
+                                    + "\n3.계산 기록 보기" + "\n4.계산기 종료\n"
+                                    + "\n원하시는 메뉴를 선택해 숫자를 입력해주세요: ");
                             menu = inputManager.inputNumber("menuNumber");
                             continue;
                         }
                         case 2: {
-                            resultManager.removeAnswer(0);
+                            try {
+                                resultManager.removeAnswer(0);
+                                System.out.println("첫번째 계산 기록을 삭제합니다.");
+                            } catch (IndexOutOfBoundsException e) {
+                                System.out.println("삭제할 기록이 없습니다.");
+                            }
                             continue;
                         }
                         case 3: {
@@ -78,10 +75,10 @@ public class App {
                             continue;
                         }
                     }
-                }
+
                 case 4: {
                     exit = 1;
-                continue;
+                    continue;
                 }
             }
 
@@ -91,7 +88,7 @@ public class App {
 
             // 두번째 숫자 입력
             System.out.print("두번째 숫자를 입력하세요: ");
-            while(true) {
+            while (true) {
                 number2 = inputManager.inputNumber("secondNumber");
                 if (symbol.equals("/") && number2 == 0) {
                     System.out.print("0으로는 나눌수 없습니다. 다른 숫자를 입력해주세요: ");
@@ -104,14 +101,20 @@ public class App {
             Operation op = Operation.formSymbol(symbol);
             result = op.calculate(number1, number2);
 
+            // 정수일 경우 소수점 표시 제거
+            displayResult = numberFormat.changeType(result);
+            String displayNumber1 = numberFormat.changeType(number1);
+            String displayNumber2 = numberFormat.changeType(number2);
+
             // 계산 결과 송출 및 저장
-            System.out.println("\n계산 결과는 " + result + "입니다.\n");
-            resultManager.addAnswer(number1 + symbol + number2 + " = " + result);
+            System.out.println("\n계산 결과는 " + displayResult + "입니다.\n" +
+                    "(소수점 아래 세자리까지만 표시됩니다)\n");
+            resultManager.addAnswer(displayNumber1 + symbol + displayNumber2 + " = " + displayResult);
 
 
             // 메뉴 선택
-            System.out.print("1.새로운 계산       " + "2.이어서 계산\n"
-                    + "3.계산 기록 보기   " + "4.계산기 종료\n"
+            System.out.print("1.새로운 계산" + "\n2.이어서 계산"
+                    + "\n3.계산 기록 보기   " + "\n4.계산기 종료\n"
                     + "\n원하시는 메뉴를 선택해 숫자를 입력해주세요: ");
             menu = inputManager.inputNumber("menuNumber");
 
