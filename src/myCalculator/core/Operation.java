8365a2b67188cc8cb1cb0cf82f0d5c363cb571a2
Ha-minsang <gkalstkd@gmail.com>
2025-10-20 12:52:07 +0900

    주석 추가

--- src/myCalculator/core/Operation.java
+++ src/myCalculator/core/Operation.java
@@ -18,25 +18,29 @@ public enum Operation {
     String symbol;
     DoubleBinaryOperator operator;
 
+    // Enum 상수 구성
     Operation(String symbol, DoubleBinaryOperator operator) {
         this.symbol = symbol;
         this.operator = operator;
     }
 
+    // 게터로 Enum 상수의 심볼 가져오기
     public String getSymbol() {
         return symbol;
     }
 
+    // 계산 메서드
     public double calculate(double a, double b) {
         return operator.applyAsDouble(a, b);
     }
 
+    // 심볼과 일치하는 심볼을 가진 Enum 상수의 람다식 가져오기
     public static Operation formSymbol(String symbol) {
         for(Operation operation : Operation.values()) {
             if(operation.getSymbol().equals(symbol)) {
                 return operation;
             }
         }
-        throw new IllegalArgumentException("지원하지 않는 연산 기호입니다.");
+        return null; // 실행되지 않지만 오류 방지를 위해 추가
     }
 }
