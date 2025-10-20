0ec385aa9801f359ae4f427a4aed021d9d2a7c78
Ha-minsang <gkalstkd@gmail.com>
2025-10-20 12:50:41 +0900

    첫번째 숫자와 두번째 숫자 입력 받는 케이스를 기능이 동일하여 하나로 통합
    문자 입력 메서드를 케이스 별로 구분하여 연산 기호를 입력받을 때와 검색 필터를 입력받을때로 구분
    
    주석 추가

--- src/myCalculator/core/InputManager.java
+++ src/myCalculator/core/InputManager.java
@@ -6,16 +6,16 @@ import java.util.Scanner;
 public class InputManager {
     Scanner sc = new Scanner(System.in);
 
+    // 숫자 입력 메서드
     public double inputNumber(String numberType) {
         while (true) {
             try {
                 switch (numberType) {
-                    case "firstNumber":
+                    // 단순 숫자 입력
+                    case "inputNumber":
                         return sc.nextDouble();
 
-                    case "secondNumber":
-                        return sc.nextDouble();
-
+                        // 메뉴 선택
                     case "menuNumber":
                         while (true) {
                             double choice = sc.nextDouble();
@@ -24,8 +24,10 @@ public class InputManager {
                             } else {
                                 System.out.print("1~4 중 다시 입력해주세요: ");
                             }
+                            break;
                         }
                 }
+                // 숫자가 아닌 값을 입력했을때
             } catch (InputMismatchException e) {
                 if (sc.hasNextLine()) {
                     sc.nextLine();
@@ -36,17 +38,36 @@ public class InputManager {
         }
     }
 
-    public String inputOperation() {
+    // 문자 입력 메서드
+    public String inputString(String input) {
         sc.nextLine();
         while (true) {
-            String choice = sc.nextLine();
-            if (choice.equals("+") || choice.equals("-")
-                    || choice.equals("*") || choice.equals("/")) {
-                return choice;
-            } else {
-                System.out.println("\n정상적인 입력이 아닙니다.");
-                System.out.print("연산 기호를 다시 입력해 주세요: ");
-            }
+            switch (input) {
+                // 기호 입력
+                case "operation": {
+                    String choice = sc.nextLine();
+                    if (choice.equals("+") || choice.equals("-")
+                            || choice.equals("*") || choice.equals("/")) {
+                        return choice;
+                    } else {
+                        System.out.println("\n정상적인 입력이 아닙니다.");
+                        System.out.print("연산 기호를 다시 입력해 주세요: ");
+                    }
+                    break;
+                }
+
+                // 검색 필터 입력
+                case "searchType": {
+                    String search = sc.nextLine();
+                    if (search.equals("over")|| search.equals("under")|| search.equals("equal")) {
+                        return search;
+                    } else {
+                        System.out.println("\n정상적인 입력이 아닙니다.");
+                        System.out.print("over / under / equal 중 다시 입력해주세요: ");
+                    }
+                    break;
+                }
+        }
         }
     }
 }
